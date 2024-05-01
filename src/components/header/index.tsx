509ecef2c9346d7f43e932b60153ae7769644c42
Igor <iigorfelipe@gmail.com>
2024-05-01 17:55:14 -0300

    ✨ implements Header component

---
+++ src/components/header/index.tsx
@@ -0,0 +1,63 @@
+import { MouseEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { Box, IconButton, Popover } from '@mui/material';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import ArrowBackOutlinedIcon from '@mui/icons-material/ArrowBackOutlined';
+import PopoverContent from '../popoverContent';
+
+const Header = () => {
+  const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = useState<HTMLButtonElement | null>(null);
+
+  const handleClick = (event: MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+
+  return (
+    <Box sx={{ display: 'flex', p: '10px' }}>
+
+      {true && (
+        <IconButton onClick={() => navigate('/')}>
+          <ArrowBackOutlinedIcon />
+        </IconButton>
+      )}
+  
+      <Box sx={{ ml: 'auto' }}>
+
+        <IconButton aria-describedby={id} onClick={handleClick}>
+          <SettingsOutlinedIcon />
+        </IconButton>
+
+        <Popover
+          id={id}
+          open={open}
+          anchorEl={anchorEl}
+          onClose={handleClose}
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'left', 
+          }}
+          transformOrigin={{
+            vertical: 'top',
+            horizontal: 'right',
+          }}
+        >
+          <PopoverContent />
+        </Popover>
+
+      </Box>
+  
+    </Box>
+  );
+};
+
+export default Header;
