8826027982113a135cb6ef7bed64cb784751aba6
Igor <iigorfelipe@gmail.com>
2024-05-28 20:36:47 -0300

    ✨ add new type

--- src/types/filters.ts
+++ src/types/filters.ts
@@ -1,7 +1,13 @@
-import { Parcela } from "./data";
+import { Parcela, ParcelasPorMesAno } from "./data";
 
 export type DadosDaFiltragem = {
   total: number;
   periodo: string;
   parcelasFiltradas: Parcela[];
+};
+
+export type DadosDaFiltragemGeral = {
+  total: number;
+  periodo: string;
+  parcelasFiltradas: ParcelasPorMesAno[];
 };
