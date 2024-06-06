2b574d995c6a7d11cef73c0f76367e2a9ab73904
Igor <iigorfelipe@gmail.com>
2024-06-06 14:31:14 -0300

    ✨ cache API data for reuse

--- src/api/index.ts
+++ src/api/index.ts
@@ -18,17 +18,8 @@ type ExchangeRatesResponse = {
 
 // const newUrl = "https://economia.awesomeapi.com.br/last/BRL-USD,BRL-EUR,BRL-AUD,BRL-CAD,BRL-JPY,BRL-CNY,BRL-GBP,USD-BRL,USD-EUR,USD-JPY,USD-CAD,USD-AUD,USD-CNY,USD-GBP,CAD-BRL,CAD-USD,CAD-EUR,EUR-BRL,EUR-GBP,EUR-JPY,EUR-AUD,EUR-CAD,EUR-CNY,EUR-USD,GBP-BRL,GBP-USD,GBP-EUR,JPY-BRL,JPY-USD,JPY-EUR,AUD-BRL,AUD-USD,AUD-EUR,CNY-BRL,CNY-USD,CNY-EUR"
 const API_URL = 'https://economia.awesomeapi.com.br/last/';
-// const CACHE_KEY = 'taxasDeCambio';
-// const CACHE_TIME_KEY = 'taxasDeCambioTime';
 
 export const obterTaxasDeCambio = async (moedaOrigem: string, moedaDestino: string): Promise<ExchangeRatesResponse | null> => {
-  // const now = new Date().getTime();
-  // const cacheTime = localStorage.getItem(CACHE_TIME_KEY);
-  // const cachedRates = localStorage.getItem(CACHE_KEY);
-
-  // if (cachedRates && cacheTime && (now - parseInt(cacheTime) < 3600000)) {
-  //   return JSON.parse(cachedRates);
-  // }
 
   try {
     const url = `${API_URL}${moedaOrigem}-${moedaDestino}`;
@@ -39,8 +30,6 @@ export const obterTaxasDeCambio = async (moedaOrigem: string, moedaDestino: stri
     };
 
     const data: ExchangeRatesResponse = await response.json();
-    // localStorage.setItem(CACHE_KEY, JSON.stringify(data));
-    // localStorage.setItem(CACHE_TIME_KEY, now.toString());
     return data;
 
   } catch (error) {
--- src/fetchers/getExchangeRate.ts
+++ src/fetchers/getExchangeRate.ts
@@ -1,16 +1,32 @@
 import { obterTaxasDeCambio } from "../api";
 
+const CACHE_DURATION = 5 * 60 * 1000; // 5 minutos;
+
 export const obterTaxaDecambio = async (moedaOrigem: string, moedaDestino: string) => {
+  const chaveTaxa = `${moedaOrigem}${moedaDestino}`;
+  const cachedData = localStorage.getItem(chaveTaxa);
+
+  if (cachedData) {
+    const parsedData = JSON.parse(cachedData);
+    const cachedTime = parsedData.timestamp;
+
+    if (Date.now() - cachedTime < CACHE_DURATION) {
+      return parseFloat(parsedData.data[`${moedaOrigem}${moedaDestino}`].bid);
+    };
+  };
+
   const taxas = await obterTaxasDeCambio(moedaOrigem, moedaDestino);
   if (!taxas) {
     return null;
   };
 
-  const chaveTaxa = `${moedaOrigem}${moedaDestino}`;
+
   const taxaOrigemDestino = parseFloat(taxas[chaveTaxa].bid);
   if (!taxaOrigemDestino) {
     return null;
   };
+
+  localStorage.setItem(chaveTaxa, JSON.stringify({ data: taxas, timestamp: Date.now() }));
 
   return taxaOrigemDestino;
 };
